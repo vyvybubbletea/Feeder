4f644299b4c9802692dc9045d57c03e3400d6fbd
Ivy Li <ivyl68@nycstudents.net>
2025-09-17 15:35:06 -0400

    more code

--- Main.java
+++ Main.java
@@ -4,13 +4,9 @@ public class Main
     {
         Feeder f = new Feeder(500);
         System.out.println(f);
-        System.out.println("Current Food: " + f.getCurrentFood());
-        f.simulateOneDay(12);
-        System.out.println("Current Food: " + f.getCurrentFood());
-        f = new Feeder(1000);
-        f.simulateOneDay(22);
-        System.out.println("Current Food: " + f.getCurrentFood());
-        f = new Feeder(2400);
-        System.out.println(f.simulateManyDays(10,4)+ " days");
+        f = new Feeder(250);
+        System.out.println(f.simulateManyDays(10,5)+ " day(s)");        f = new Feeder(0);
+        f = new Feeder(0);
+        System.out.println(f.simulateManyDays(5,10)+ " day(s)");
     }
 }
